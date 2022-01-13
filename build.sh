c5fd758f4644793de5e8f6bc624cee7482ab05f6
techcto <smoore@solodev.com>
2022-01-13 07:37:29 -0500

    ..

---
+++ build.sh
@@ -0,0 +1,23 @@
+
+#- IE: ./build.sh php/alpine/php-fpm-7.2 techcto alpine-php-fpm-7.2
+IMAGE=$2/$3
+
+docker build --tag $IMAGE:latest $1/.
+
+if [ -n "$4" ]; then
+  docker build --tag $IMAGE:$4 $1/.
+else
+  echo "No tag supplied"
+fi
+
+docker save $IMAGE | docker run  -v /tmp -i myyk/docker-squash -t $IMAGE -verbose | docker load
+
+docker push $IMAGE:latest
+if [ -n "$4" ]; then
+  docker push $IMAGE:$4
+else
+  echo ""
+fi
+
+echo "After:"
+docker images $IMAGE
